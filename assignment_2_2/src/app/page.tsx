b49578f82debdf9c53dea97511eebcdc056d26fb
Curtis Kauer <kauercurtis@gmail.com>
2024-04-15 15:44:51 -0700

    week 3 in class with assignment 2

--- assignment_2_2/src/app/page.tsx
+++ assignment_2_2/src/app/page.tsx
@@ -1,35 +1,9 @@
-'use client';
 
-import { Container, List, ListItem, 
-          TableContainer, Table, TableHead, 
-          TableBody, TableRow, TableCell 
-        } from "@mui/material";
+import { Container, List, ListItem, TableContainer } from "@mui/material";
 
-import { styled } from "@mui/material/styles";
-import { tableCellClasses } from "@mui/material/TableCell";
+import MyTable from "./components/myTable";
 
-const StyledTableCell = styled(TableCell)(() => ({
-  [`&.${tableCellClasses.head}`] : {
-    backgroundColor: "black", 
-    color: "white",
-    fontSize: 16, 
-    textAlign: "center"
-  },
-  [`&.${tableCellClasses.body}`] : {
-    fontSize: 14, 
-    textAlign: "center"
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(() => ({
-  "&:nth-of-type(even)":{
-    backgroundColor: "#ccc"
-  },
-  "&:nth-of-type(odd)":{
-    backgroundColor: "#ADD8E6"
-  }
-}));
-
+//To Do: Encapuslate the data into its own file. JSON file comes to mind. 
 
 export default function Home() {
   const columns: string [] = ["Game Title", "Developer", "Year", "Genre", "Platform"];
@@ -40,46 +14,14 @@ export default function Home() {
     ["Portal 2", "Valve", "2011", "Puzzle", "PC, Playstation 3, Xbox 360"], 
     ["Tetris", "Alexey Pajitnov", "1984", "Puzzle", "Varying Platforms"], 
     ["Grand Theft Auto V", "Rockstar North", "2013", "Open World", "Varying Platforms"]
-  ]
-
-  function HeaderRow(){
-    return(
-      <StyledTableRow>
-        <StyledTableCell>{columns[0]}</StyledTableCell>
-        <StyledTableCell>{columns[1]}</StyledTableCell>
-        <StyledTableCell>{columns[2]}</StyledTableCell>
-        <StyledTableCell>{columns[3]}</StyledTableCell>
-        <StyledTableCell>{columns[4]}</StyledTableCell>
-      </StyledTableRow>
-    );
-  }
-
-  function newRow(rowNumber: number){
-    return(
-      <StyledTableRow>
-        <TableCell>{gameData[rowNumber][0]}</TableCell>
-        <TableCell>{gameData[rowNumber][1]}</TableCell>
-        <TableCell>{gameData[rowNumber][2]}</TableCell>
-        <TableCell>{gameData[rowNumber][3]}</TableCell>
-        <TableCell>{gameData[rowNumber][4]}</TableCell>
-      </StyledTableRow>
-    );
-  }
-
+  ];
+    
   return (
-    <main>
-      <TableContainer>
-        <Table>
-          <TableHead>{HeaderRow()}</TableHead>
-          <TableBody>
-            {newRow(0)}
-            {newRow(1)}
-            {newRow(2)}
-            {newRow(3)}
-            {newRow(4)}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </main>
-  );
-}
+      <main>
+        <TableContainer>
+          <MyTable columns={columns} gameData={gameData}/>
+        </TableContainer>
+      </main>
+    );
+  }
+
