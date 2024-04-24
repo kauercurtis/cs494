adeb7bf7071298a0b7144a2264b8ef35c0c73243
Curtis Kauer <kauercurtis@gmail.com>
2024-04-24 15:39:44 -0700

    another inclass

--- assignment_2_2/src/app/page.tsx
+++ assignment_2_2/src/app/page.tsx
@@ -1,47 +1,21 @@
-import {TableContainer } from "@mui/material";
-
-import MyTable from '../components/myTable';
 
 import { promises as fs } from "fs";
 
-export default async function Home() {
+import MyPage from '../components/myPage';
 
-  const fileData = await fs.readFile(process.cwd() + "/data/star_data.json", "utf8") 
+export default async function Home(){
 
-  const jsonData: {name: string,
-                    distance_from_earth_ly: number,
-                    size: string, 
-                    mass: string, 
-                    temperature: string, 
-                    type: string 
-                    } = JSON.parse(fileData);
+  const fileData = await fs.readFile(process.cwd() + "/data/movie_data.json", "utf8");
 
-  const headers: string[] = ["name", "distance from earth ly", "size", "mass", "temperature", "type"];
-  
-  // TODO Convert the data to objects instead.
-  // {title: string, year: string, director: string, genre: string[], score: string}
-  // const data: string[][] = [
-  //   ["The Dark Knight", "2008", "Christopher Nolan", "Action, Crime, Drama", "94%"],
-  //   ["The Shawshank Redemption", "1994", "Frank Darabont", "Drama", "98%"],
-  //   ["Inception", "2010", "Christopher Nolan", "Action, Adventure, Sci-Fi", "91%"],
-  //   ["Pulp Fiction", "1994", "Quentin Tarantino", "Crime, Drama", "96%"],
-  //   ["Forrest Gump", "1994", "Robert Zemeckis", "Drama, Romance", "95%"]
-  // ];
-
-  // const data: {name: string, distance_from_earth_ly: string, size: string, mass: string[], temperature: string, type: string}[] = [];
-
-  // let foo: string = "";
-
-  // jsonData.forEach(data.push({name: foo.name, distance_from_earth_ly: foo.distance_from_earth_ly, size: foo.size, mass: foo.mass, temperature: foo.temperature, type: foo.type}));
-
-  // data.push({title: "The Dark Knight", year: "2008", director: "Christopher Nolan", genre: ["Action", "Crime", "Drama"], score: "94%"});
-
-  
+  const jsonData: {title: string, 
+                  release_year: number,
+                  director: string,
+                  genre: string[],
+                  starring_actors: string[],
+                  rotten_tomatoes_score: number}[] = JSON.parse(fileData)
   return (
     <main>
-      <TableContainer>
-        <MyTable headers={headers} data={jsonData} />
-      </TableContainer>
+      <MyPage jsonData={jsonData}/>
     </main>
   );
 }
